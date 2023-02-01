a5367de1191576ace01e849eaceac56f278074c6
Leandro Arturi <lea.arturi@gmail.com>
2023-02-01 18:12:24 -0300

    getCategories()

---
+++ README.md
@@ -0,0 +1,32 @@
+# Petgram
+
+### React App
+
+## 🚀 Back
+
+Run the development server:
+
+```bash
+cd api
+npm install
+```
+
+```bash
+npm run dev:api
+
+Listening at http://localhost:3500/graphql
+```
+
+## 🚀 Front
+
+Run the development app:
+
+```bash
+npm install
+```
+
+```bash
+npm run dev
+```
+
+##### Made with ❤️ by Leandro Arturi
--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,18 +1,44 @@
 /* eslint-disable react/jsx-key */
 
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { getApiBaseUrl } from '../../hooks/useConfig'
 import { Category } from '../Category'
-import { categories } from '../../../api/db.json'
 import { List, Item } from './styles'
 
-export const ListOfCategories = () => (
-  <List>
-    {
+export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+
+  const urlApi = getApiBaseUrl()
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const response = await window.fetch(`${urlApi}/categories`)
+      const data = await response.json()
+      setCategories(data)
+    }
+
+    fetchCategories()
+  }, [])
+
+  // useEffect(() => {
+  //   fetch(`${urlApi}/categories`)
+  //     .then(res => res.json())
+  //     .then(response => {
+  //       setCategories(response)
+  //     })
+  // }, [])
+
+  return (
+
+    <List>
+      {
         categories.map(category => (
           <Item key={category.id}>
             <Category {...category} />
           </Item>
         ))
     }
-  </List>
-)
+    </List>
+
+  )
+}

b2b6619691a8051d5c890bf076a815356d4d3752
Leandro Arturi <lea.arturi@gmail.com>
2023-02-03 06:24:01 -0300

    Like Button

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { MdFavoriteBorder } from 'react-icons/md'
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 import { Button, ImgWrapper, Img, Article } from './styles'
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
@@ -9,17 +9,37 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
 
   const [show, setShow] = useState(false)
 
+  const key = `like-${id}`
+
+  const [liked, setLiked] = useState(() => {
+    try {
+      return window.localStorage.getItem(key)
+    } catch (error) {
+      return false
+    }
+  })
+
   useEffect(() => {
     const observer = new window.IntersectionObserver(function (entries) {
       const { isIntersecting } = entries[0]
       if (isIntersecting) {
-        console.log('Si')
         setShow(true)
         observer.disconnect()
       }
     })
     observer.observe(element.current)
   }, [element])
+
+  const Icon = liked ? MdFavorite : MdFavoriteBorder
+
+  const setLocalStorage = (value) => {
+    try {
+      window.localStorage.setItem(key, value)
+      setLiked(value)
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   return (
     <Article ref={element}>
@@ -32,8 +52,8 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
             </ImgWrapper>
           </a>
 
-          <Button>
-            <MdFavoriteBorder size='32px' />{likes} likes!
+          <Button onClick={() => setLocalStorage(!liked)}>
+            <Icon size='32px' />{likes} likes!
           </Button>
         </>
       )

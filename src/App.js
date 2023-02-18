a218d3bb158db770c28956830d973faf7c56477d
Leandro Arturi <lea.arturi@gmail.com>
2023-02-18 12:43:15 -0300

    PhotoCardWithQuery detail

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 import { Logo } from './components/Logo'
+import { PhotoCardWithQuery } from './containers/PhotoCardWithQuery'
 import { GlobalStyles } from './GlobalStyles'
 
-export const App = () => (
-  <>
-    <Logo />
-    <GlobalStyles />
-    <ListOfCategories />
-    <ListOfPhotoCards categoryId={3} />
-  </>
-)
+export const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search)
+  const detailId = urlParams.get('detail')
+  console.log(detailId)
+  return (
+    <>
+      <Logo />
+      <GlobalStyles />
+
+      {
+        detailId
+          ? (<PhotoCardWithQuery id={detailId} />)
+          : (
+            <>
+              <ListOfCategories />
+              <ListOfPhotoCards categoryId={3} />
+            </>
+            )
+      }
+
+    </>
+  )
+}

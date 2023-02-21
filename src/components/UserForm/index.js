a90a618ff53d0ef37456468e0405dbb365094a5e
Leandro Arturi <lea.arturi@gmail.com>
2023-02-21 10:52:27 -0300

    Register Mutation

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -1,20 +1,31 @@
 import React from 'react'
 import { useInputValue } from '../../hooks/useInputValue'
-import { Button, Form, Input, Title } from './styles'
+import { Button, Error, Form, Input, Title } from './styles'
 
-export const UserForm = ({ onSubmit, title }) => {
+export const UserForm = ({ onSubmit, title, error, disabled }) => {
   const email = useInputValue('')
   const password = useInputValue('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    onSubmit({
+      email: email.value,
+      password: password.value
+    })
+  }
 
   return (
     <div style={{ padding: '20px' }}>
       <Title>{title}</Title>
 
-      <Form onSubmit={onSubmit}>
-        <Input placeholder='Email' type='text' {...email} />
-        <Input placeholder='Password' type='password' {...password} />
-        <Button>{title}</Button>
+      <Form onSubmit={handleSubmit} disabled={disabled}>
+        <Input placeholder='Email' disabled={disabled} type='text' {...email} />
+        <Input placeholder='Password' disabled={disabled} type='password' {...password} />
+        <Button disabled={disabled}>{title}</Button>
       </Form>
+      {
+        error && <Error>{error}</Error>
+      }
     </div>
   )
 }
--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,18 +1,39 @@
 import React from 'react'
 import { UserForm } from '../components/UserForm'
 import Context from '../Context'
+import { useRegisterMutation } from '../containers/RegisterMutation'
 
-export const NotRegisteredUser = () => (
-  <Context.Consumer>
-    {
-      ({ activateAuth }) => {
-        return (
-          <>
-            <UserForm title='Registrarse' onSubmit={activateAuth} />
-            <UserForm title='Iniciar sesión' onSubmit={activateAuth} />
-          </>
-        )
-      }
+export const NotRegisteredUser = () => {
+  const { registerMutation, loading, error } = useRegisterMutation()
+
+  return (
+    <Context.Consumer>
+      {
+        ({ activateAuth }) => {
+          const onSubmit = ({ email, password }) => {
+            const input = { email, password }
+            const variables = { input }
+            registerMutation({ variables }).then(activateAuth)
+          }
+
+          const errorMsg = error && 'El usuario ya existe'
+
+          return (
+            <>
+              <UserForm
+                disabled={loading}
+                error={errorMsg}
+                title='Registrarse'
+                onSubmit={onSubmit}
+              />
+              <UserForm
+                title='Iniciar sesion'
+                onSubmit={activateAuth}
+              />
+            </>
+          )
+        }
     }
-  </Context.Consumer>
-)
+    </Context.Consumer>
+  )
+}

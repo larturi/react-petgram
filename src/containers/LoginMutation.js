a3e22de6861f518b3358c91ab028aa0eee857522
Leandro Arturi <lea.arturi@gmail.com>
2023-02-21 11:02:25 -0300

    Login Mutation

---
+++ src/containers/LoginMutation.js
@@ -0,0 +1,11 @@
+import { gql, useMutation } from '@apollo/client'
+
+const LOGIN = gql`
+  mutation login($input: UserCredentials!) {
+    login (input: $input)
+  }
+`
+export const useLoginMutation = () => {
+  const [login, { loading, error }] = useMutation(LOGIN)
+  return { login, loading, error }
+}
--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -2,33 +2,43 @@ import React from 'react'
 import { UserForm } from '../components/UserForm'
 import Context from '../Context'
 import { useRegisterMutation } from '../containers/RegisterMutation'
+import { useLoginMutation } from '../containers/LoginMutation'
 
 export const NotRegisteredUser = () => {
-  const { registerMutation, loading, error } = useRegisterMutation()
-
+  const { registerMutation, loading: loadingRegister, error: errorRegister } = useRegisterMutation()
+  const { login, loading: loadingLogin, error: errorLogin } = useLoginMutation()
   return (
     <Context.Consumer>
       {
         ({ activateAuth }) => {
-          const onSubmit = ({ email, password }) => {
+          const onSubmitRegister = ({ email, password }) => {
             const input = { email, password }
             const variables = { input }
             registerMutation({ variables }).then(activateAuth)
           }
 
-          const errorMsg = error && 'El usuario ya existe'
+          const onSubmitLogin = ({ email, password }) => {
+            const input = { email, password }
+            const variables = { input }
+            login({ variables }).then(activateAuth)
+          }
+
+          const errorMsgRegister = errorRegister && 'El usuario ya existe'
+          const errorMsgLogin = errorLogin && 'Las credenciales son incorrectas'
 
           return (
             <>
               <UserForm
-                disabled={loading}
-                error={errorMsg}
+                disabled={loadingRegister}
+                error={errorMsgRegister}
                 title='Registrarse'
-                onSubmit={onSubmit}
+                onSubmit={onSubmitRegister}
               />
               <UserForm
+                disabled={loadingLogin}
+                error={errorMsgLogin}
                 title='Iniciar sesion'
-                onSubmit={activateAuth}
+                onSubmit={onSubmitLogin}
               />
             </>
           )
